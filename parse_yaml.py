1cd45ae16284ed819658a95afcc12e35a5477929
Corey Angers <corey@numerated.coml>
2018-06-08 18:21:55 -0400

    make it a class

--- parse_yaml.py
+++ parse_yaml.py
@@ -4,37 +4,48 @@ import os
 import sys
 
 class ParseYaml(object):
+	"""
+	Script to read yaml file and create bash variable assignments as such:
+	node1_node2_..._noden=leaf
+	Which are written to stdout
+	"""
 
-    def __init__(self, file, sep, cap, set):
-        self.file = file
-        self.sep = sep
-        self.cap = cap
-        self.set = set
-        self.data = self.parse_yaml(self.file)
+	def __init__(self, file, sep, cap, set):
+		self.file = file
+		self.sep = sep
+		self.cap = cap
+		self.set = set
+		self.data = self.parse_yaml(self.file)
+		self.res = self.top_level()
 
-    def walk_dict(self, indict, pre=None):
-        for key, value in indict.items():
-            if isinstance(value, dict):
-                pre = "{}_{}".format(pre, key) if pre is not None else key
-                walk_dict(value, pre=pre)
-            else:
-                bash_var = "{}_{}".format(pre, key) if pre is not None else key
-                bash_assignment = "{}={}".format(bash_var, value)
-                return(bash_assignment)
+	def walk_dict(self, indict, pre=None):
+		for key, value in indict.items():
+			print key, value
+			if isinstance(value, dict):
+				pre = "{}_{}".format(pre, key) if pre is not None else key
+				self.walk_dict(value, pre=pre)
+			else:
+				bash_var = "{}_{}".format(pre, key) if pre is not None else key
+				bash_assignment = "{}={}".format(bash_var, value)
+				return(bash_assignment)
 
-    def parse_yaml(self, file):
-        if not os.path.isfile(file):
-            raise IOError('File {} not found'.format(file))
-        with open(file, 'r') as yml:
-            data = yaml.load(yml)
-        return(data)
+	def top_level(self):
+		res = []
+		for key, value in self.data.items():
+			if key is None:
+				continue
+			res += self.walk_dict({key: value})
+		return res
 
-    def test(self):
-        res = []
-        for key, value in self.data.items():
-            res += walk_dict({key: value})
-            print(res)
-        return res
+	def parse_yaml(self, file):
+		if not os.path.isfile(file):
+			raise IOError('File {} not found'.format(file))
+		with open(file, 'r') as yml:
+			data = yaml.load(yml)
+		return(data)
+
+	def get(self, key, default):
+		return self.data.get(key, default)
 
 if __name__ == '__main__':
-    test()
+	ParseYaml('')
